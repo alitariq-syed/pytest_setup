cb042f35e49845d0a3fa72881630ccf7b3ffccd4
Syed Ali Tariq <alitariq.bsc06@gmail.com>
2023-04-06 22:36:41 +0500

    more asserts

--- my_app/Tests/test_my_models.py
+++ my_app/Tests/test_my_models.py
@@ -8,6 +8,7 @@ print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 from my_scripts.my_models import infer_mmseg_model, initialize_mmseg_model
 import cv2
 import pytest
+import numpy as np
 
 @pytest.fixture()
 def load_demo_img():
@@ -21,7 +22,6 @@ def test_model_init():
     segmentation_model = initialize_mmseg_model()
     # print(segmentation_model)
     assert segmentation_model is not None
-    return segmentation_model
 
 def test_model_inference(load_demo_img, load_model):
     img = load_demo_img
@@ -29,3 +29,5 @@ def test_model_inference(load_demo_img, load_model):
     result = infer_mmseg_model(model,img)
     print("result: ",result)
     assert result is not None
+    assert isinstance(result, list)
+    assert result[0].dtype == np.int64
