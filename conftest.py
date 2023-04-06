6cdce8e62fad78042566e2736973d49c0312d3d8
Syed Ali Tariq <alitariq.bsc06@gmail.com>
2023-04-07 00:37:20 +0500

    more examples

--- conftest.py
+++ conftest.py
@@ -1,4 +1,42 @@
 import sys
+import pytest
+import cv2
+from my_app.my_scripts.my_models import infer_mmseg_model, initialize_mmseg_model
+
+
 
 def pytest_sessionstart(session):
     sys.path.append("my_app")
+
+@pytest.fixture()
+def load_demo_img():
+    return cv2.imread("my_app/Tests/Test_images/demo.png")
+
+#with a scope of "module", the fixture initializes the model once and returns it for use in the test functions.
+model = initialize_mmseg_model()
+@pytest.fixture(scope="module")
+def mmseg_model():
+    return model
+
+@pytest.fixture()
+def empty_img():
+    return None
+
+@pytest.fixture(params=[("my_app/Tests/Test_images/demo.png", "test_mask1.png"),
+                        ("test_image2.png", "test_mask2.png"),
+                        ("test_image3.png", "test_mask3.png")])
+def test_multiple_image_pairs(request):
+    # Load the test image and mask
+    image = cv2.imread(request.param[0])
+    mask = cv2.imread(request.param[1])
+    
+    return (image, mask)
+
+# @pytest.fixture(params=["my_app/Tests/Test_images/demo.png",
+#                         "my_app/Tests/Test_images/demo.png",
+#                         "my_app/Tests/Test_images/demo.png"])
+# def test_multiple_images(request):
+#     # Load the test image and mask
+#     image = cv2.imread(request.param)
+    
+#     return image
--- my_app/Tests/test_my_models.py
+++ my_app/Tests/test_my_models.py
@@ -3,35 +3,69 @@ import cv2
 import pytest
 import numpy as np
 
-@pytest.fixture()
-def load_demo_img():
-    return cv2.imread("my_app/Tests/Test_images/demo.png")
-
-@pytest.fixture()
-def load_model():
-    return initialize_mmseg_model()
-
-@pytest.fixture()
-def empty_img():
-    return None
-
 def test_model_init():
     segmentation_model = initialize_mmseg_model()
     # print(segmentation_model)
     assert segmentation_model is not None
 
-def test_model_inference(load_demo_img, load_model):
+def test_model_inference(load_demo_img, mmseg_model):
     img = load_demo_img
-    model = load_model
-    result = infer_mmseg_model(model,img)
+    result = infer_mmseg_model(mmseg_model,img)
     print("result: ",result)
     assert result is not None
     assert isinstance(result, list)
     assert result[0].dtype == np.int64
 
-def test_empty_image(empty_img, load_model):
+def test_empty_image(empty_img, mmseg_model):
     img = empty_img
-    model = load_model
     with pytest.raises(TypeError): #, match="my error message"
-        infer_mmseg_model(model,img)
-  
+        infer_mmseg_model(mmseg_model,img)
+
+mmseg_model = initialize_mmseg_model()
+
+@pytest.mark.parametrize("image_path", [
+    "my_app/Tests/Test_images/demo.png",
+    "my_app/Tests/Test_images/demo.png",
+    "my_app/Tests/Test_images/demo.png",
+])
+def test_multiple_image_inference(image_path, mmseg_model):
+    img = cv2.imread(image_path)
+    result = infer_mmseg_model(mmseg_model,img)
+    print("result: ",result)
+    assert result is not None
+    assert isinstance(result, list)
+    assert result[0].dtype == np.int64
+
+
+# import pytest
+# from my_module import load_image, load_mask
+
+# @pytest.fixture(scope="module")
+# def model():
+#     model = load_model("path/to/model")
+#     return model
+
+# @pytest.fixture(scope="function")
+# def image(request):
+#     image_path = request.param
+#     # Load the image using the provided path
+#     image = load_image(image_path)
+#     return image
+
+# @pytest.fixture(scope="function")
+# def mask(request):
+#     mask_path = request.param
+#     # Load the mask using the provided path
+#     mask = load_mask(mask_path)
+#     return mask
+
+# @pytest.mark.parametrize("image, mask", [
+#     ("path/to/image1.jpg", "path/to/mask1.jpg"),
+#     ("path/to/image2.jpg", "path/to/mask2.jpg"),
+#     ("path/to/image3.jpg", "path/to/mask3.jpg"),
+# ], indirect=True)
+# def test_function(model, image, mask):
+#     # Use the model to perform predictions on the image
+#     predictions = model.predict(image)
+#     # Your test code here
+#     pass
